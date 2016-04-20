84c06be94e9ea7e25c618f84f727e68ea3489e19
tehdslayer <srosario2018@bklawtech.com>
2016-04-20 21:22:07 +0000

    Weather

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-window.onload = function () { console.log('loaded!') };
+window.onload = function (){
+	var formElement = document.getElementById('form');
+	var inputElement = document.getElementById('input');
+
+	formElement.onsubmit = function(event){
+		event.preventDefault();
+		var city = inputElement.value;
+		var zipCode = inputElement.value;
+		getWeather(city);
+	};
+};
+
+function printWeather(event){
+	var response = JSON.parse(event.target.responseText);
+	document.write(response.main.temp);
+}
+
+function getWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?' + 
+  'appid=c2f2d170f6f6fc336058e9851edb828c'+
+  '&q=' + city +
+  '&units=imperial';
+
+  var xhrObject = new XMLHttpRequest();
+  xhrObject.open('GET', url, true);
+  xhrObject.onload = printWeather;
+  xhrObject.send();
